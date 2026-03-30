883556158ddb51dcf319ea7c640f7198728d1e4c
Cedric <cedric.coussement@gmail.com>
2026-03-30 21:58:04 +0200

    2de methode geschreven voor monster

--- dungeonGame.Test/MonstersTests.cs
+++ dungeonGame.Test/MonstersTests.cs
@@ -8,7 +8,14 @@ public class MonstersTests
     public void TakeDamade_Reduce_Health_To_Zero()
     {
         Monster monster = new Monster("Goblin", 80, 15);
-        monster.TakeDamage(100);
+        monster.TakeDamage(70);
+        Assert.AreEqual(0, monster.Health);
+    }
+    [Test]
+    public void Take_Damage_Ignores_Negative_Damage()
+    {
+        Monster monster = new Monster("orc", 100,15);
+        monster.TakeDamage (-25);
         Assert.AreEqual(100, monster.Health);
     }
 }
