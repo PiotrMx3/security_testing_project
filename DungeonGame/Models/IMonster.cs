ae05c70e51046960203a7b2da008cfa6e1366ab8
Cedric <cedric.coussement@gmail.com>
2026-04-05 10:57:38 +0200

    2 extra interfaces aangemaakt IRoom,IMonster

---
+++ DungeonGame/Models/IMonster.cs
@@ -0,0 +1,14 @@
+﻿namespace DungeonGame
+{
+    public interface IMonster
+    {
+        int AttackDamage { get; set; }
+        int Health { get; set; }
+        bool IsAlive { get; }
+        string Name { get; set; }
+        bool RequiresWeapon { get; set; }
+
+        void Attack(IPlayer player);
+        void TakeDamage(int amount);
+    }
+}
