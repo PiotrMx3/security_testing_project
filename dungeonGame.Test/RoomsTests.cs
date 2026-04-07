ce4e83113881962fa06f3e9469ca0092a7564deb
Cedric <cedric.coussement@gmail.com>
2026-04-07 07:52:08 +0200

    Eerste test van rooms gemaakt

--- dungeonGame.Test/RoomsTests.cs
+++ dungeonGame.Test/RoomsTests.cs
@@ -8,22 +8,23 @@ public class RoomsTests
     [Test]
     public void Move_Monster_alive_and_Block_exit_PlayerDies()
     {
-        var mockPlayer = new Mock<IPlayer>();
-        var mockInventory = new Mock<IInventory>();
+        var player = new Player("TestPlayer", 100);
         var mockMonster = new Mock<IMonster>();
-        mockPlayer.Setup(p => p.Inventory).Returns(mockInventory.Object);
-        mockPlayer.Setup(p => p.Health).Returns(100);
+
         mockMonster.Setup(m => m.IsAlive).Returns(true);
 
         var startRoom = new Room("Start", "Starting room", blockExitIfMonsterAlive: true);
         var nextRoom = new Room("Next", "Next room");
+
         startRoom.Monster = mockMonster.Object;
         startRoom.AddExit(Direction.North, nextRoom);
 
         var rooms = new Rooms(new List<Room> { startRoom, nextRoom }, startRoom);
-        rooms.Move(Direction.North, mockPlayer.Object);
 
-        mockPlayer.Verify(p => p.TakeDamage(100), Times.Once);
+        rooms.Move(Direction.North, player);
+
+        Assert.AreEqual(0, player.Health);
+        Assert.AreEqual(startRoom, rooms.CurrentRoom);
     }
 
 }
