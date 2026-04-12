80a28a090575b4e8e018e23281fc082d7d296c32
Piotr Mulinski <piotr.mulinski@icloud.com>
2026-04-12 22:36:54 +0200

    fis: test class name

--- dungeonGame.Test/DirectionHelper/DirectionHelperParseTest.cs
+++ dungeonGame.Test/DirectionHelper/DirectionHelperParseTest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DungeonGame.Test.DirectionHelper
 {
-    public class DirectionHelperDirectionTest
+    public class DirectionHelperParseTest
     {
         [TestCase("n")]
         [TestCase("N")]
