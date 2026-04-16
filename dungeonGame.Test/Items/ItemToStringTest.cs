15b9fc6b931e382578a6a897bb44f41629a13d69
Cedric De Clippeleir <c3k90@icloud.com>
2026-04-16 11:37:24 +0200

    refactor file setup

--- dungeonGame.Test/Items/ItemToStringTest.cs
+++ dungeonGame.Test/Items/ItemToStringTest.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DungeonGame.Test
+namespace DungeonGame.Test.Items
 {
     [TestFixture]
-    internal class ItemTest
+    internal class ItemToStringTest
     {
         [Test]
         public void ToString_WhenCalled_ReturnsFormattedString()
@@ -18,7 +18,7 @@ namespace DungeonGame.Test
             string result = item.ToString();
             // 3. ASSERT
             // De tekst moet exact zijn: $"{Name} ({Type}): {Description}";
-            Assert.That(result, Is.EqualTo("Sleutel (Key): Een gouden sleutel"));
+
         }
     }
 }
