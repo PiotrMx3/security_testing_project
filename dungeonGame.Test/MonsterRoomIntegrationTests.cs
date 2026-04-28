0cb8d82a53c394987be751a7adff1d07a22ec66c
Cedric <cedric.coussement@gmail.com>
2026-04-28 08:48:21 +0200

    Monster integratieTest

---
+++ dungeonGame.Test/MonsterRoomIntegrationTests.cs
@@ -0,0 +1,31 @@
+namespace DungeonGame.Test;
+
+[TestFixture]
+public class MonsterRoomIntegrationTests
+{
+    [Test]
+    public void Integration_PlayerCanLeaveMonsterRoomAfterKillingTheMonster()
+    {
+        //arrange
+        var previousRoom = new Room("Previous room", "Safe room", false, false, null, false);
+        var monsterRoom = new Room("Monster room", "testing", false, false, null,true); 
+        var monster = new Monster("Orc", 1, 0, false);
+        var player = new Player("Testhero", 100);
+        monsterRoom.Monster = monster;
+        monsterRoom.AddExit(Direction.South, previousRoom);
+        previousRoom.AddExit(Direction.North,monsterRoom);
+        var rooms = new Rooms(new List<IRoom> { monsterRoom, previousRoom }, monsterRoom);
+
+        //act
+        rooms.Fight(player);
+        bool result = rooms.Move(Direction.South, player);
+
+        //assert
+
+
+        Assert.IsTrue(monsterRoom.Monster.IsAlive == false);
+        Assert.IsTrue(result);
+        Assert.AreEqual(previousRoom, rooms.CurrentRoom);
+
+    }
+}
